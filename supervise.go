1632d708758589046754b0b528957c4c69563ab3
nbari <nbari@tequila.io>
2016-07-09 00:53:56 +0200

    modified:   supervise.go

--- supervise.go
+++ supervise.go
@@ -14,16 +14,18 @@ func (self *Daemon) Supervice() {
 		select {
 		case m := <-self.monitor:
 			content, err := ioutil.ReadFile(self.Pidfile)
+
 			if err != nil {
 				Log(Yellow(fmt.Sprintf("monitor: %s", m.Error())))
 			}
+
 			lines := strings.Split(string(content), "\n")
 			pid, err := strconv.Atoi(lines[0])
 			if err != nil {
 				Log(Red(fmt.Sprintf("Bad process id found in %s, %s", self.Pidfile, err.Error())))
 			}
 
-			Log(Green(fmt.Sprintf("PID on file: %v", pid)))
+			Log(Green(fmt.Sprintf("PID on file: %v Parent Pid: %d", pid, self.pid)))
 			time.Sleep(1 * time.Second)
 			go self.Monitor()
 		case err := <-self.status:
