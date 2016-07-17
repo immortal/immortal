cd55204b1868ed191dec71e3a67d6df2756e9130
nbari <nbari@tequila.io>
2016-07-17 22:16:18 +0200

    new file:   pidfile.go 	modified:   run.go 	modified:   supervise.go

--- run.go
+++ run.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	//	"os"
 	"os/exec"
 	"strconv"
 	"sync/atomic"
--- supervise.go
+++ supervise.go
@@ -2,25 +2,8 @@ package immortal
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
 	"time"
 )
-
-// readPidfile read pid from file if error returns pid 0
-func (self *Daemon) readPidfile() (int, error) {
-	content, err := ioutil.ReadFile(self.run.FollowPid)
-	if err != nil {
-		return 0, err
-	}
-	lines := strings.Split(string(content), "\n")
-	pid, err := strconv.Atoi(lines[0])
-	if err != nil {
-		return 0, err
-	}
-	return pid, nil
-}
 
 func (self *Daemon) Supervice() {
 	self.Run(self.ctrl.state)
