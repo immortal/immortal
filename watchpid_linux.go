8e4a4744d3db6ecbfa713ae98a209d2eda7e7dab
nbari <nbari@tequila.io>
2016-07-19 11:21:29 +0200

    modified:   watchpid_linux.go

--- watchpid_linux.go
+++ watchpid_linux.go
@@ -10,7 +10,7 @@ import (
 
 func (self *Daemon) watchPid(ch chan<- error) {
 	for {
-		if _, err := os.Stat(fmt.Sprintf("/proc/%d", self.pid); os.IsNotExist(err) {
+		if _, err := os.Stat(fmt.Sprintf("/proc/%d", self.pid)); os.IsNotExist(err) {
 			ch <- fmt.Errorf("EXIT")
 		}
 		time.Sleep(time.Second)
