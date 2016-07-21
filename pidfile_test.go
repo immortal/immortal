d0013ec4ad2d6fb3996e63560a62d4ff7073ef99
nbari <nbari@tequila.io>
2016-07-21 21:25:21 +0200

    modified:   pidfile_test.go

--- pidfile_test.go
+++ pidfile_test.go
@@ -1,9 +1,98 @@
 package immortal
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestreadPidfile(t *testing.T) {}
+func TestReadPidfileNonexistent(t *testing.T) {
+	D := &Daemon{
+		run: Run{
+			FollowPid: "nonexistent",
+		},
+	}
+	i, e := D.readPidfile()
+	if i != 0 {
+		t.Errorf("Expecting: 0 got: %v", i)
+	}
+	if e == nil {
+		t.Errorf("Expecting: no such file or directory")
+	}
+}
 
-func TestwritePid(t *testing.T) {}
+func TestReadPidfileBadContent(t *testing.T) {
+	D := &Daemon{
+		run: Run{
+			FollowPid: "pidfile_test.go",
+		},
+	}
+	i, e := D.readPidfile()
+	if i != 0 {
+		t.Errorf("Expecting: 0 got: %v", i)
+	}
+	if e == nil {
+		t.Errorf("Expecting: no such file or directory")
+	}
+}
+
+func TestReadPidfile(t *testing.T) {
+	content := []byte("1234")
+	tmpfile, err := ioutil.TempFile("", "TestReadPidfile")
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Error(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Error(err)
+	}
+	D := &Daemon{
+		run: Run{
+			FollowPid: tmpfile.Name(),
+		},
+	}
+	i, e := D.readPidfile()
+	if i != 1234 {
+		t.Errorf("Expecting: 1234 got: %v", i)
+	}
+	if e != nil {
+		t.Error(e)
+	}
+}
+
+func TestWritePidNonexistent(t *testing.T) {
+	D := &Daemon{}
+	err := D.writePid("/dev/null/nonexistent", 1234)
+	if err == nil {
+		t.Error("Expecting an error")
+	}
+}
+
+func TestWritePid(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "TestWritePid")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(tmpfile.Name()) // clean up
+	D := &Daemon{
+		run: Run{
+			FollowPid: tmpfile.Name(),
+		},
+	}
+	err = D.writePid(tmpfile.Name(), 1234)
+	if err != nil {
+		t.Error(err)
+	}
+	i, e := D.readPidfile()
+	if i != 1234 {
+		t.Errorf("Expecting: 1234 got: %v", i)
+	}
+	if e != nil {
+		t.Error(e)
+	}
+}
