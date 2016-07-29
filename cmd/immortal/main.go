cab45143c66e4c82c745dfa5473f50b9c033277c
nbari <nbari@tequila.io>
2016-07-29 20:16:23 +0200

    modified:   cmd/immortal/main.go

--- cmd/immortal/main.go
+++ cmd/immortal/main.go
@@ -17,7 +17,7 @@ func main() {
 	// flag set
 	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-v -ctrl] [-d dir] [-e dir] [-f pidfile] [-l logfile] [-logger logger] [-p child_pidfile] [-P supervisor_pidfile] [-u user] command\n\n   command\n        The command with arguments if any, to supervise.\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [-v -ctrl] [-d dir] [-e dir] [-f pidfile] [-l logfile] [-logger logger] [-p child_pidfile] [-P supervisor_pidfile] [-u user] command\n\n   command\n        The command with arguments if any, to supervise\n\n", os.Args[0])
 		fs.PrintDefaults()
 	}
 
