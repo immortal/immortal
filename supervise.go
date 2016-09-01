bdb7e527ad63d247754b5c2e93ba1f88cae1087a
nbari <nbari@tequila.io>
2016-09-01 20:28:37 +0200

    removed extra select

--- supervise.go
+++ supervise.go
@@ -51,55 +51,51 @@ func Supervise(d *Daemon) {
 				run <- struct{}{}
 			}
 			s = &Sup{p}
-		default:
-			select {
-			case err := <-p.errch:
-				// unlock, or lock once
-				atomic.StoreUint32(&d.lock, d.lock_once)
-				if err != nil && err.Error() == "EXIT" {
-					log.Printf("PID: %d Exited", pid)
-				} else {
-					log.Printf("PID %d terminated, %s [%v user  %v sys  %s up]\n",
-						p.cmd.ProcessState.Pid(),
-						p.cmd.ProcessState,
-						p.cmd.ProcessState.UserTime(),
-						p.cmd.ProcessState.SystemTime(),
-						time.Since(p.sTime),
-					)
-					// calculate time for next reboot (avoids high CPU usage)
-					uptime := p.eTime.Sub(p.sTime)
-					wait = 0 * time.Second
-					if uptime < time.Second {
-						wait = time.Second - uptime
-					}
+		case err := <-p.errch:
+			// unlock, or lock once
+			atomic.StoreUint32(&d.lock, d.lock_once)
+			if err != nil && err.Error() == "EXIT" {
+				log.Printf("PID: %d Exited", pid)
+			} else {
+				log.Printf("PID %d terminated, %s [%v user  %v sys  %s up]\n",
+					p.cmd.ProcessState.Pid(),
+					p.cmd.ProcessState,
+					p.cmd.ProcessState.UserTime(),
+					p.cmd.ProcessState.SystemTime(),
+					time.Since(p.sTime),
+				)
+				// calculate time for next reboot (avoids high CPU usage)
+				uptime := p.eTime.Sub(p.sTime)
+				wait = 0 * time.Second
+				if uptime < time.Second {
+					wait = time.Second - uptime
 				}
-
-				// follow the new pid and stop running the command
-				// unless the new pid dies
-				if d.cfg.Pid.Follow != "" {
-					pid, err = s.ReadPidFile(d.cfg.Pid.Follow)
-					if err != nil {
-						log.Printf("Cannot read pidfile:%s, %s", d.cfg.Pid.Follow, err)
-						run <- struct{}{}
-					} else {
-						// check if pid in file is valid
-						if pid > 1 && pid != p.Pid() && s.IsRunning(pid) {
-							log.Printf("Watching pid %d on file: %s", pid, d.cfg.Pid.Follow)
-							s.WatchPid(pid, p.errch)
-						} else {
-							// if cmd exits or process is kill
-							run <- struct{}{}
-						}
-					}
-				} else {
-					run <- struct{}{}
-				}
-			case fifo := <-d.fifo:
-				if fifo.err != nil {
-					log.Printf("control error: %s", fifo.err)
-				}
-				s.HandleSignals(fifo.msg, d)
 			}
+			// follow the new pid and stop running the command
+			// unless the new pid dies
+			if d.cfg.Pid.Follow != "" {
+				pid, err = s.ReadPidFile(d.cfg.Pid.Follow)
+				if err != nil {
+					log.Printf("Cannot read pidfile:%s, %s", d.cfg.Pid.Follow, err)
+					run <- struct{}{}
+				} else {
+					// check if pid in file is valid
+					if pid > 1 && pid != p.Pid() && s.IsRunning(pid) {
+						log.Printf("Watching pid %d on file: %s", pid, d.cfg.Pid.Follow)
+						s.WatchPid(pid, p.errch)
+					} else {
+						// if cmd exits or process is kill
+						run <- struct{}{}
+					}
+				}
+			} else {
+				run <- struct{}{}
+			}
+		case fifo := <-d.fifo:
+			if fifo.err != nil {
+				log.Printf("control error: %s", fifo.err)
+			}
+			s.HandleSignals(fifo.msg, d)
 		}
 	}
 }
