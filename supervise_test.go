e6636cbf941ae634e9a19350339daee87a8612d7
nbari <nbari@tequila.io>
2016-09-04 10:19:06 +0200

    2100 wait timeout

--- supervise_test.go
+++ supervise_test.go
@@ -133,7 +133,7 @@ func TestSupervise(t *testing.T) {
 	default:
 		for sup.IsRunning(watchPid) {
 			// wait mock watchpid to finish
-			time.Sleep(1500 * time.Millisecond)
+			time.Sleep(2100 * time.Millisecond)
 		}
 		newchild_pid_after, err := sup.ReadPidFile(filepath.Join(parentDir, "child.pid"))
 		if err != nil {
