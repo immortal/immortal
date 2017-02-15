38a46671ac798a8ec3728ab659c0c542a05a5053
nbari <nbari@tequila.io>
2017-02-15 15:48:23 +0100

    modified:   cmd/immortal-ctl/main.go

--- cmd/immortal-ctl/main.go
+++ cmd/immortal-ctl/main.go
@@ -12,23 +12,6 @@ var version string
 // immortal-ctl status (print status of all services)
 func main() {
 	var (
-		u     = flag.Bool("u", false, "If the service is not running, `start` it.")
-		d     = flag.Bool("d", false, "If the service is running, `stop` it.")
-		a     = flag.Bool("a", false, "alarm")
-		c     = flag.Bool("c", false, "cont")
-		h     = flag.Bool("hup", false, "hup")
-		i     = flag.Bool("i", false, "int")
-		in    = flag.Bool("in", false, "TTIN")
-		k     = flag.Bool("k", false, "kill")
-		o     = flag.Bool("o", false, "once")
-		out   = flag.Bool("out", false, "out")
-		s     = flag.Bool("s", false, "stop")
-		q     = flag.Bool("q", false, "quit")
-		t     = flag.Bool("t", false, "term")
-		usr1  = flag.Bool("1", false, "usr1")
-		usr2  = flag.Bool("2", false, "usr2")
-		winch = flag.Bool("w", false, "winch")
-
 		v    = flag.Bool("v", false, fmt.Sprintf("Print version: %s", version))
 		sdir string
 	)
@@ -40,12 +23,30 @@ func main() {
 	}
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "usage: %s [options] [signal] [sdir] service\n\n%s\n%s\n%s\n%s\n\n",
+		fmt.Fprintf(os.Stderr, "usage: %s [option] [signal] service\n\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
 			os.Args[0],
 			"  Options:",
 			"    start     Start the service",
 			"    stop      Stop the service",
-			"    restart   Restart the service")
+			"    restart   Restart the service",
+			"    once      Do not restart if service stops",
+			"    kill      Terminate service",
+			"  Signals:",
+			"    a, alrm   ALRM",
+			"    c, cont   CONT",
+			"    d, down   TERM",
+			"    h, hup    HUP",
+			"    i, int    INT",
+			"    in, TTIN  TTIN",
+			"    ou, TTOU  TTOU",
+			"    s, stop   STOP",
+			"    q, quit   QUIT",
+			"    t, term   TERM",
+			"    q, quit   QUIT",
+			"    1, usr1   USR1",
+			"    2, usr2   USR2",
+			"    w, winch  WINCH",
+		)
 		flag.PrintDefaults()
 	}
 
@@ -55,8 +56,4 @@ func main() {
 		fmt.Printf("%s\n", version)
 		os.Exit(0)
 	}
-	fmt.Printf("*u = %+v\n", *u)
-	fmt.Printf("*d = %+v\n", *d)
-	println(a, c, h, i, in, k, o, out, s, q, t, usr1, usr2, winch)
-
 }
