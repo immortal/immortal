dc83f4c7512cb879882333b77864603e41370e19
nbari <nbari@tequila.io>
2017-09-07 16:48:32 +0200

    loggerstderr test

--- logger_test.go
+++ logger_test.go
@@ -3,8 +3,36 @@ package immortal
 import (
 	"io/ioutil"
 	"log"
+	"os"
 	"testing"
 )
+
+func TestNewStderrLogger(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "stderr")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	log.SetOutput(ioutil.Discard)
+	cfg := &Config{
+		Stderr: Log{
+			File: tmpfile.Name(),
+		},
+	}
+	l := NewStderrLogger(cfg)
+	expect(t, true, l != nil)
+}
+
+func TestNewStderrLoggerFileNone(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	cfg := &Config{
+		Stderr: Log{
+			File: "/dev/null/nonexist",
+		},
+	}
+	l := NewStderrLogger(cfg)
+	expect(t, true, l == nil)
+}
 
 func TestNewLoggerFileNone(t *testing.T) {
 	log.SetOutput(ioutil.Discard)
