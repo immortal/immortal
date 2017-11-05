d9bdea5b67f3e9544c973cdf418f5c1e69165b83
nbari <nbari@tequila.io>
2017-11-05 12:04:44 +0100

    scandir tests

--- scandir_test.go
+++ scandir_test.go
@@ -2,7 +2,13 @@
 
 package immortal
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
 
 func TestNewScanDir(t *testing.T) {
 	_, err := NewScanDir("/tmp")
@@ -15,5 +21,85 @@ func TestNewScanDirNonexistent(t *testing.T) {
 	_, err := NewScanDir("/dev/null/non-existent")
 	if err == nil {
 		t.Error("Expecting error")
+	}
+}
+
+// TestWathFile create a dummy file, do changes on it and WathFile should return
+// the same file
+func TestWathFile(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestWathFile")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+	s, err := NewScanDir(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	file := filepath.Join(dir, "run.yml")
+	if err = ioutil.WriteFile(file, []byte(""), 0644); err != nil {
+		t.Fatal(err)
+	}
+	go s.WatchFile(file)
+	go func() {
+		time.Sleep(time.Second)
+		if err = ioutil.WriteFile(file, []byte("--"), 0644); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	select {
+	case watch := <-s.watch:
+		expect(t, file, watch)
+	case <-time.After(time.Second * 2):
+		t.Fatal("time out waiting for file to change")
+	}
+}
+
+type mockController struct{}
+
+func (mc *mockController) GetStatus(socket string) (*Status, error)                  { return &Status{}, nil }
+func (mc *mockController) SendSignal(socket, signal string) (*SignalResponse, error) { return nil, nil }
+func (mc *mockController) FindServices(dir string) ([]*ServiceStatus, error)         { return nil, nil }
+func (mc *mockController) PurgeServices(dir string) error                            { return nil }
+func (mc *mockController) Run(command string) ([]byte, error) {
+	// TODO
+	return nil, nil
+}
+
+func TestScandir(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestWathFile")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+	s, err := NewScanDir(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dir, err = filepath.EvalSymlinks(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	file := filepath.Join(dir, "run.yml")
+	if err = ioutil.WriteFile(file, []byte(""), 0644); err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		time.Sleep(time.Second)
+		if err = ioutil.WriteFile(file, []byte("--"), 0644); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	ctl := &mockController{}
+	s.Scandir(ctl)
+	select {
+	case watch := <-s.watch:
+		expect(t, file, watch)
+	case <-time.After(time.Second * 2):
+		t.Fatal("time out waiting for file to change")
+	}
+	_, ok := s.services.Load("run.yml")
+	if ok {
+		t.Fatalf("Not expecting value for %s", "run.yml")
 	}
 }
